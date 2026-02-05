38d0345e010acceab5090b2c26fd9b515d49314b
Oleksandr Panchenko <pan.pan.4it@gmail.com>
2026-02-05 20:46:38 +0200

    ls19

---
+++ lesson20_pars/lesson_20_xpath.py
@@ -0,0 +1,10 @@
+logo_xpath = '//div[@id="loginSection"]/div/h2'
+logo_xpath_2 = '//h2[@class="text-3xl font-bold text-gray-900 mb-6 text-center"]'
+
+username = '//*[@id="username"]'
+password = '//*[@id="password"]'
+sign_in_button = '//div[@class="space-y-4"]/button'
+sign_in_button_2 = '//button[@onclick="login()"]'
+sign_in_button_3 = "//button[contains(@class,\"w-full bg-indigo-600\")]"
+tex_on_page = "//p[contains(.,'Complete value of ‘Type’')]" # . or text()
+cars_count = '//*[@id="carsList"]/div'
